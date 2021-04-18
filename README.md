39c9f1d7f12cecaffeae72a554e11a0efbe45a94
MaheepChaudhary <chaudhary.maheep28@gmail.com>
2021-04-19 02:19:59 +0530

    material_drawer

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ mtc stands for ml_to_cloud, the project is focused on building a a ml system whi
 :anchor: <font size = "6"> **Tasks** </font>
 - [ ]  Learn [Flutter](https://www.youtube.com/watch?v=RGdg-TCmldY)
 > - [x] ~~[Setting up](https://www.youtube.com/watch?v=w7lu5sOM9LU) Flutter on my machine~~  
-> - [ ] [Crash Course](https://www.youtube.com/watch?v=1gDhl4leEzA) on flutter
+> - [ ] [Crash Course](https://www.youtube.com/watch?v=Ib2FlirtcmE) on flutter
 > - [ ] Do some ```Code``` by putting up user signup page
 > - [ ]  Learn about [Flutter Firebase Authentication](https://www.youtube.com/watch?v=oJ5Vrya3wCQ)
--- mtc/lib/main.dart
+++ mtc/lib/main.dart
@@ -3,12 +3,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MaterialApp(
     title: "Awesome App",
     home: Homepage(),
-  ));
+    theme: ThemeData(
+      primarySwatch: Colors.green
+    ),
+  )
+  );
 }
 
 class Homepage extends StatelessWidget {
@@ -18,37 +23,43 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("firstapp"),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child :Align(alignment: Alignment.bottomRight,
-        child : Container(
-          color: Colors.black,
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child : Row(
-            mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.red
-              ),
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.green
-                  ),
-              Container(
-                  width: 100,
-                  height: 100,
-                  color: Colors.blue
-                ),
-             ],
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget> [
+          //DrawerHeader(
+            //child: Text("I am drawer",style: TextStyle(color: Colors.white),),
+            //decoration: BoxDecoration(
+              //color: Colors.pink
+            //),
+          //),
+        UserAccountsDrawerHeader(
+            accountName: Text("Maheep Chauhdary"),
+            accountEmail: Text("chaudhary.maheep28@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage("https://avatars.githubusercontent.com/u/51289356?s=400&u=a7d9301de6b856b93d859a12482f4209b582c3a7&v=4"),
             ),
-          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.person),
+          title: Text("Accounts"),
+          subtitle: Text("Maheep Chaudhary"),
+          trailing: Icon(Icons.edit)
+        ),
+        ListTile(
+            leading: Icon(Icons.person),
+            title: Text("Password"),
+            subtitle: Text("********"),
+            trailing: Icon(Icons.send)
+        ),
+          ],
         ),
       ),
+      body: Container(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+          onPressed: (){} ,
+          child: Icon(Icons.edit),),
     );
   }
 }
